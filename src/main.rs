f66184c8988dcf962a3497aaf2babc847c939b21
Sgeo <sgeoster@gmail.com>
2017-06-20 20:48:40 -0400

    Start progress. Add lock file.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate image;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = std::env::args().nth(1).expect("No filename provided!");
+    let image = image::open(&filename).expect("Unable to open image!");
 }
